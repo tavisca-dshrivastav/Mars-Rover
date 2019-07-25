87c564e4bcc723ad2caaedf6897dc33d3956bde8
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-07-26 02:23:37 +0530

    Added helper method to invoke function

--- MarsRover.Test/SurfaceCordinateFixture.cs
+++ MarsRover.Test/SurfaceCordinateFixture.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MarsRover.Test
@@ -13,6 +14,21 @@ namespace MarsRover.Test
             _surfaceCordinate = new SurfaceCordinate(4, 5);
             _surfaceCordinate.XCordinate.Should().Be(4);
             _surfaceCordinate.YCordinate.Should().Be(5);
+        }
+    }
+    public class MarsRoverFixture
+    {
+        [Fact]
+        public void Test_on_actual_input()
+        {
+            List<string> input = new List<string>
+            {
+                "5 5",
+                "1 2 N",
+                "LFLFLFLFF"
+            };
+            var helper = new Helper();
+            helper.ProcessInput(input).Should().Be("1 3 N");
         }
     }
 }
---
+++ MarsRover/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace MarsRover
+{
+    public class Helper
+    {
+        public string ProcessInput(List<string> input)
+        {
+            //token1 is in format of {Max_X Max_Y}
+            var token1 = input[0].Split(' ');
+            var lastValidPosition = new SurfaceCordinate(int.Parse(token1[0]), int.Parse(token1[1]));
+
+            //token2 is in format of {current_x current_y direction}
+            var token2 = input[1].Split(' ');
+            var currentPosition = new SurfaceCordinate(int.Parse(token2[0]), int.Parse(token2[1]));
+
+            var direction = new Direction(token2[2]);
+            var map = new Map(currentPosition, lastValidPosition);
+
+            //token3 is a string of instruction 
+            //e.g. LLFFLL
+            var token3 = input[2];
+            var rover = new Rover(map, direction);
+
+
+            return rover.ExecuteInstruction(token3);
+
+
+        }
+    }
+}
