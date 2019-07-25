7b8581189e344a9ea4a9f0a78b31a2092d81900a
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-07-26 01:57:37 +0530

    Added Rover Support

---
+++ MarsRover.Test/RoverFixture.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MarsRover.Test
+{
+    public class RoverFixture
+    {
+        [Fact]
+        public void Rover_should_have_map_and_direction_and_execute_instruction()
+        {
+            var map = new Map(new SurfaceCordinate(2, 4), new SurfaceCordinate(5, 5));
+            var direction = new Direction("N");
+            var rover = new Rover(map, direction);
+            rover.ExecuteInstruction("LLRF").Should().Be("1 4 W");
+        }
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,27 @@
+﻿namespace MarsRover
+{
+    public class Rover
+    {
+        public Rover(Map map, Direction direction)
+        {
+            _map = map;
+            _direction = direction;
+        }
+        private Map _map;
+        private Direction _direction;
+
+        public string ExecuteInstruction(string Instructions)
+        {
+            foreach(var command in Instructions)
+            {
+                if (command == 'L')
+                    _direction.OnMoveLeft();
+                if (command == 'R')
+                    _direction.OnMoveRight();
+                if (command == 'F')
+                    _direction.OnMoveForward(_map);
+            }
+            return $"{_map.CurrentPosition.XCordinate} {_map.CurrentPosition.YCordinate} {_direction.CurrentDirection}";
+        }
+    }
+}
