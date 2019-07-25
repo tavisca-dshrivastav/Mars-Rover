77f034fdf3f73b5b7f653bd8a65a8c22f6e8ef53
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-07-26 02:28:12 +0530

    Moved Different classes to their respective .cs file

---
+++ MarsRover.Test/MarsRoverFixture.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MarsRover.Test
+{
+    public class MarsRoverFixture
+    {
+        [Fact]
+        public void Test_on_actual_input()
+        {
+            List<string> input = new List<string>
+            {
+                "5 5",
+                "1 2 N",
+                "LFLFLFLFF"
+            };
+            var helper = new Helper();
+            helper.ProcessInput(input).Should().Be("1 3 N");
+        }
+    }
+}
